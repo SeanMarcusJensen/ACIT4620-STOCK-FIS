62974964163c5f7032560f7670c9fc594cf218ed
Sean Marcus Jensen <itskolesean@gmail.com>
2024-11-16 14:16:24 +0100

    storing files

--- models/stock.py
+++ models/stock.py
@@ -1,13 +1,13 @@
-from utils import download_history
+from utils import download_stock_data
 
 class Stock:
-    def __init__(self, ticker, start=None, end=None, period='1d', interval='1m', **kwargs):
+    def __init__(self, ticker, **kwargs):
         self.name = ticker
-        self.__data = download_history(ticker,
-                                       start=start, end=end,
-                                       period=period, interval=interval,
-                                       **kwargs)
+        self.__data = download_stock_data(ticker, **kwargs)
 
     def __getitem__(self, key):
         return self.__data[key] 
 
+if __name__ == "__main__":
+    STOCK = Stock("AMZN", interval='5m')
+    print(STOCK["Close"])
