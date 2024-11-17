f2237f1dea9e5c883e363509a37b9331b9fe1229
Sean Marcus Jensen <67327992+SeanMarcusJensen@users.noreply.github.com>
2024-11-17 14:10:55 +0100

    Chunking dataframes into days. (#7)
    * indexing by date
    
    * yes
    
    * yers
    
    * reqs

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
-__all__ = ['download_stock_data']
+__all__ = ['download_stock_data', 'DateChunker']
 from .download_stock_data import download_stock_data
+from .date_chunker import DateChunker
---
+++ utils/date_chunker.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from typing import Dict
+
+
+class DateChunker:
+    def __init__(self, data: pd.DataFrame, index: str = 'Datetime', column: str = 'Date') -> None:
+        self.chunked: Dict[str, pd.DataFrame] = {}
+        self.columns = data.columns
+
+        data = data.reset_index()
+        data[column] = pd.to_datetime(data[index], utc=True).dt.date
+        data.set_index(index, inplace=True)
+        chunks = data.groupby(column)
+        for key, value in chunks:
+            self.chunked[key.__str__()] = pd.DataFrame(value[self.columns])
+
+    def get_date(self, date: str) -> pd.DataFrame:
+        data = self.chunked.get(date, pd.DataFrame())
+        return data
+
+    def get_date_chunk(self, start: str, end: str) -> pd.DataFrame:
+        data = pd.concat([self.get_date(date)
+                         for date in self.chunked.keys() if start <= date <= end])
+        return data
+
+    def __iter__(self):
+        return iter(self.chunked.values())
+
+
+if __name__ == "__main__":
+    from models import Stock
+    STOCK = Stock("AAPL", interval='5m')
+    chunker = DateChunker(STOCK.get_data())
+    chunk = chunker.get_date('2024-11-01')
+    print(chunk.head())
